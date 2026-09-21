b20238355f21486bcb23626722c21024ca3a1fdf
AgentKevin2077 <agentkevin2077@gmail.com>
2026-09-21 14:28:13 +0800

    Point portal at the deployed Apps Script web app.

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 /* 部署 Web App 後，把網址貼到 webAppUrl。 */
 window.MH_CONFIG = {
   masterId: "1lBDzpsWRfRiL2Bi1wR5jkeDEfZJKPz-loS3m24q0esc",
-  webAppUrl: "",
+  webAppUrl: "https://script.google.com/macros/s/AKfycbwQFoNBBgJ-VUOf8Pudcn2HHhwKIrMWwP8wsAe4Bt6S8PapuPkd6ZNyhifMMA4Fulnv/exec",
   portalUrl: "https://me0wx-lr.github.io/magitech-hearts-school-portal/"
 };
